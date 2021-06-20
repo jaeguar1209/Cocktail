177a018616478189cd404a0f8f96f899d806e622
subin <sea1498@pusan.ac.kr>
2021-06-21 03:46:53 +0900

    illustration

--- ASD/app/src/main/java/com/example/asd/SearchMain.java
+++ ASD/app/src/main/java/com/example/asd/SearchMain.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -12,6 +13,7 @@ public class SearchMain extends Activity {
     Button button2;
     TextInputEditText textInputEditText1, textInputEditText2;
     Button SearchBtn1, SearchBtn2;
+    ImageView imageView10,imageView11,imageView12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -20,9 +22,16 @@ public class SearchMain extends Activity {
         textInputEditText1 = findViewById(R.id.ingredient);
 
         textInputEditText2 = findViewById(R.id.cocktailName);
+        imageView10 = findViewById(R.id.imageView10);
+        imageView11= findViewById(R.id.imageView11);
+        imageView12 = findViewById(R.id.imageView12);
 
         SearchBtn1 = (Button)findViewById(R.id.SearchBtn1);
         SearchBtn2 = (Button)findViewById(R.id.SearchBtn2);
+
+        imageView10.setImageResource(R.drawable.illu1);
+        imageView11.setImageResource(R.drawable.illu2);
+        imageView12.setImageResource(R.drawable.illu3);
 
 
         SearchBtn1.setOnClickListener(new View.OnClickListener() {
